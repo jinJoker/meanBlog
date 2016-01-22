5ecc989ac6759ee6b938be504c47aa7f8f4ec4ca
unknown <I300575@PVGN50906791A.apj.global.corp.sap>
2016-01-22 11:43:19 +0800

    merge

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
 
- app.get('/', function(req, res) {
-      res.send('hello world');
+app.use('/js', express.static(__dirname + '/client/js'));
+app.use('/css', express.static(__dirname + '/client/css'));
+
+app.get('/', function(req,res){
+    res.sendfile(__dirname + '/client/views/index.html');
 });
+
 
 app.listen(3000, function(){
     console.log('I\'m listening');
